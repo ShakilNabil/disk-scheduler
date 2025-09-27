5dd0e7023356f4423eb1dcd77d099bd1861bac3f
Shakil Nabil <shakilnabil11@gmail.com>
2025-09-27 17:36:16 -0400

    preferences section complete

--- js/exam.js
+++ js/exam.js
@@ -7,6 +7,7 @@ const algorithmTypes = document.getElementById("algorithm-types-section");
 const algorithmOptions = document.getElementsByName("algorithms");
 const toggleBtn = document.querySelectorAll(".toggle-btn");
 const examSettingsForm = document.getElementById("exam-settings-form");
+const typeWarning = document.getElementById("type-warning");
 
 examLayoutContainer.classList.add("hidden");
 resultsContainer.classList.add("hidden");
@@ -32,15 +33,34 @@ toggleBtn.forEach(btn => {
      });
 });
 
-
+const examConfig = {algorithm: null, totalQuestionCount: null, timeLimit: null, algorithmPorbability: null};
 examSettingsForm.addEventListener("submit", (event) => {
      event.preventDefault();
+     typeWarning.classList.add("hidden");
+     let algorithmList = []
      if(questionRandomizedInput.checked) {
           for (const algorithmOption of algorithmOptions) {
                     algorithmOption.checked = true;
+                    algorithmList.push(algorithmOption.value);
                }     
-     }     
+     }
+     else if(!questionRandomizedInput.checked){
+          const anyChecked = [...algorithmOptions].some(option => option.checked);
+          if(!anyChecked) typeWarning.classList.remove("hidden");
+          algorithmList = ([...algorithmOptions].filter(option => option.checked).map(option => option.value));
+     }
+     const numOfQuestionList = document.getElementsByName("number-of-questions");
+     const timeLimitList = document.getElementsByName("time-limit");
+     examConfig.algorithm = algorithmList;
+     examConfig.totalQuestionCount = Number([...numOfQuestionList].find(option => option.checked).value);
+     const chosenTimeLimit = [...timeLimitList].find(option => option.checked).value;
+     chosenTimeLimit == "null" ? examConfig.timeLimit = null : examConfig.timeLimit = Number(chosenTimeLimit); 
+     console.log(examConfig);
 });
+
+
+
+// ================== Exam ==================
 
 
 
