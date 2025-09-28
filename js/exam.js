8867018ef1c530351c87998a0d14afde2bfc410c
Shakil Nabil <shakilnabil11@gmail.com>
2025-09-28 15:23:47 -0400

    started creating exam UI and logic

--- js/exam.js
+++ js/exam.js
@@ -2,6 +2,7 @@
 
 const examLayoutContainer = document.getElementById("exam-layout");
 const resultsContainer = document.getElementById("results-container");
+const examSetupContainer = document.getElementById("exam-setup");
 const questionRandomizedInput = document.getElementById("question-randomize");
 const algorithmTypes = document.getElementById("algorithm-types-section");
 const algorithmOptions = document.getElementsByName("algorithms");
@@ -33,10 +34,11 @@ toggleBtn.forEach(btn => {
      });
 });
 
-const examConfig = {algorithm: null, totalQuestionCount: null, timeLimit: null, algorithmPorbability: null};
+
 examSettingsForm.addEventListener("submit", (event) => {
      event.preventDefault();
      typeWarning.classList.add("hidden");
+     const examConfig = {algorithm: null, totalQuestionCount: null, timeLimit: null, algorithmProbability: {}};
      let algorithmList = []
      if(questionRandomizedInput.checked) {
           for (const algorithmOption of algorithmOptions) {
@@ -46,7 +48,10 @@ examSettingsForm.addEventListener("submit", (event) => {
      }
      else if(!questionRandomizedInput.checked){
           const anyChecked = [...algorithmOptions].some(option => option.checked);
-          if(!anyChecked) typeWarning.classList.remove("hidden");
+          if(!anyChecked) {
+               typeWarning.classList.remove("hidden");
+               return;
+          } 
           algorithmList = ([...algorithmOptions].filter(option => option.checked).map(option => option.value));
      }
      const numOfQuestionList = document.getElementsByName("number-of-questions");
@@ -55,12 +60,29 @@ examSettingsForm.addEventListener("submit", (event) => {
      examConfig.totalQuestionCount = Number([...numOfQuestionList].find(option => option.checked).value);
      const chosenTimeLimit = [...timeLimitList].find(option => option.checked).value;
      chosenTimeLimit == "null" ? examConfig.timeLimit = null : examConfig.timeLimit = Number(chosenTimeLimit); 
-     console.log(examConfig);
+
+     let currentStart = 0;
+     const baseProbability = 1/algorithmList.length;
+     for(const algorithm of algorithmList) {
+          examConfig.algorithmProbability[algorithm] = {count: 0, weight: 1, probability: baseProbability, range: {start: currentStart, end: currentStart + baseProbability}
+          }
+          currentStart += baseProbability;
+     }
+
+     createExam(examConfig);
 });
 
 
 
 // ================== Exam ==================
 
+function createExam(examConfig) {
+     // create questions inside examConfig with number and algorithm. use loop to create numbers and create the randomize algorithm in order to randomly pick a question.
+     renderExam(examConfig);     
+}
 
+function renderExam(examConfig) {
+     examSetupContainer.classList.add("hidden");
+     examLayoutContainer.classList.remove("hidden");
+}
 
